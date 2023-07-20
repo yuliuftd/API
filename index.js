2856c85fcd3fb32f92cc5e57fe158aa907afc4a2
yuliuftd <liuyuftd@gmail.com>
2023-07-21 00:25:11 +1000

    add origin

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ const connectDB = async () => {
 connectDB();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://vitalandmiranda.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
